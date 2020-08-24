832b67ac46f30efaf07d17b23c8753b0c62f6048
rohanishervin <53121573+rohanishervin@users.noreply.github.com>
2020-08-24 13:38:24 +0000

    Update configuration.nix

--- nixos/lzf/configuration.nix
+++ nixos/lzf/configuration.nix
@@ -11,6 +11,12 @@ let
   };
   
 in {
+  imports =
+
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
