4f39e2219406127ff750d1c6f495696f41dbd6f3
Shervin Rohani <shervin.rohani72@gmail.com>
2020-08-24 09:51:02 +0000

    flake added.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Reproducible dev environment flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      lzf = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ (import ./nixos/lzf/configuration.nix) ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+
+    lzf = self.nixosConfigurations.lzf.config.system.build.toplevel;
+
+    defaultPackage.x86_64-linux =
+      (builtins.head (builtins.attrValues self.nixosConfigurations)).pkgs;
+  };
+}
